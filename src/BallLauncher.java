e9f2573a2136900abe4a99452442ad1097d20cce
huytr <m_tran80@u.pacific.edu>
2024-10-06 16:24:47 -0700

    Pause will stop the timer and the ball would stay in place

--- src/BallLauncher.java
+++ src/BallLauncher.java
@@ -27,6 +27,11 @@ public class BallLauncher extends GraphicsProgram{
 	}
 	
 	public void mousePressed(MouseEvent e) {
+		for (GOval ball : balls) {
+			if (ball.getX() < 100) {
+				return;
+			}
+		}
 		GOval ball = makeBall(SIZE/2, e.getY());
 		add(ball);
 		balls.add(ball);
