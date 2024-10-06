6850a9ea41aafdc0f93969f00a87128e37a3f23a
huytr <m_tran80@u.pacific.edu>
2024-10-06 15:59:28 -0700

    Timer needs an int variable to initialized the time the timer is run for Timer needs a function to execute at the end of the duration Timer needs a ".start()" function to execute the timer

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,11 +1,20 @@
-import acm.graphics.*;
-import acm.program.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.*;
 
-public class MyFirstTimer extends GraphicsProgram {
+import javax.swing.Timer;
+
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
+
+public class MyFirstTimer extends GraphicsProgram implements ActionListener{
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private GLabel myLabel;
+	
+	private int numTimes;
+	
+	private Timer timer = new Timer(1000, this);
 
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -13,8 +22,21 @@ public class MyFirstTimer extends GraphicsProgram {
 	}
 	
 	public void run() {
+		numTimes = 0;
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+		
+		timer.setInitialDelay(3000);
+		timer.start();
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		numTimes++;
+		myLabel.move(5, 0);
+		myLabel.setLabel("times called? " + numTimes);
+		if (numTimes == 10) {
+			timer.stop();
+		}
 	}
 	
 	public static void main(String[] args) {
